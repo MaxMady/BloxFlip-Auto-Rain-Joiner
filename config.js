8bae962525571930762e01bc6aa01e074f6428d3
MaxMady <81635616+MaxMady@users.noreply.github.com>
2022-07-09 12:52:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-    "auth":"",
-    "webhook":"",
-    "win_notif": false,
-    "product":"chrome",
-    "path":"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+    auth:"",
+    webhook:"",
+    win_notif: false,
+    product:"chrome",
+    path:"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 }
 module.exports = config;
